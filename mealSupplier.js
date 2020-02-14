97a464c397783d3f025c246b368ed728d65a5cec
freydema <freydema@users.noreply.github.com>
2020-02-14 15:14:41 +0100

    add mealplanner app

---
+++ mealSupplier.js
@@ -0,0 +1,16 @@
+function allMeals(){
+    return [
+        {name:"Spaghetti bolognese"},
+        {name:"Guacamolee"},
+        {name:"Pumpkin soup"},
+        {name:"Goat cheese salad"},
+        {name:"Chili con carne"},
+        {name:"Flamenkuche"},
+        {name:"Poulet saute a l'oignon"},
+        {name:"Pasta with mushrooms"},
+        {name:"Mushroom risotto"},
+        {name:"Salmon and cherry tomatoes"},
+        {name:"Lentil soup"},
+        {name:"Chicken and red cabbage"},
+    ]
+}
